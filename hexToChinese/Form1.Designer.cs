d3f1eac8866519e5a483b1717d76035bfda91afe
Your Name <you@example.com>
2020-12-22 12:41:13 +0800

    1.1

---
+++ hexToChinese/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace hexToChinese
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.GhostWhite;
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 215);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.GhostWhite;
+            this.textBox2.Location = new System.Drawing.Point(250, 22);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 215);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(145, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hex解码→";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(145, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "←Hex编码";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(156, 219);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "编码增加%";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GB2312",
+            "UTF-8"});
+            this.comboBox1.Location = new System.Drawing.Point(156, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(77, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(395, 260);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(411, 299);
+            this.MinimumSize = new System.Drawing.Size(411, 299);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "16进制转汉字工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ hexToChinese/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hexToChinese
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        /// <summary>
+        /// 16进制转gb2312
+        /// </summary>
+        /// <param name="hexcode"></param>
+        /// <returns></returns>
+        public static string hexTogb2312(string hexcode)
+        {
+            hexcode = hexcode.Replace("%", "");
+            byte[] array = new byte[hexcode.Length / 2];
+            for (int i = 0; i < hexcode.Length; i++)
+            {
+                try
+                {
+                    array[i] = byte.Parse(hexcode.Substring(i * 2, 2), NumberStyles.HexNumber);
+                }
+                catch
+                {
+                }
+            }
+            return Encoding.GetEncoding("gb2312").GetString(array);
+        }
+        /// <summary>
+        /// 16进制转utf-8
+        /// </summary>
+        /// <param name="hexcode"></param>
+        /// <returns></returns>
+        public static string hexToutf_8(string hexcode)
+        {
+            hexcode = hexcode.Replace("%", "");
+            byte[] array = new byte[hexcode.Length / 2];
+            for (int i = 0; i < hexcode.Length; i++)
+            {
+                try
+                {
+                    array[i] = byte.Parse(hexcode.Substring(i * 2, 2), NumberStyles.HexNumber);
+                }
+                catch
+                {
+                }
+            }
+            return Encoding.GetEncoding("utf-8").GetString(array);
+        }
+
+        /// <summary>
+        /// gb2312转16进制
+        /// </summary>
+        /// <param name="chinese"></param>
+        /// <param name="select">是否增加%号</param>
+        /// <returns></returns>
+        public static string gb2312ToHex(string chinese,bool select = false)
+        {
+            byte[] bytes = Encoding.GetEncoding("gb2312").GetBytes(chinese);
+            string text = "";
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (select)
+                {
+                    text = text + "%" + string.Format("{0:X}", bytes[i]);
+                }
+                else
+                {
+                    text = text + string.Format("{0:X}", bytes[i]);
+                }
+                
+            }
+            return text;
+        }
+        /// <summary>
+        /// utf-8转16进制
+        /// </summary>
+        /// <param name="chinese"></param>
+        /// <param name="select"></param>
+        /// <returns></returns>
+        public static string utf_8ToHex(string chinese, bool select = false)
+        {
+            byte[] bytes = Encoding.GetEncoding("utf-8").GetBytes(chinese);
+            string text = "";
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (select)
+                {
+                    text = text + "%" + string.Format("{0:X}", bytes[i]);
+                }
+                else
+                {
+                    text = text + string.Format("{0:X}", bytes[i]);
+                }
+
+            }
+            return text;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                textBox2.Text = hexTogb2312(textBox1.Text);
+            }
+            else
+            {
+                textBox2.Text = hexToutf_8(textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                if (checkBox1.Checked)
+                {
+                    textBox1.Text = gb2312ToHex(textBox2.Text, true);
+                }
+                else
+                {
+                    textBox1.Text = gb2312ToHex(textBox2.Text);
+                }
+            }
+            else
+            {
+                if (checkBox1.Checked)
+                {
+                    textBox1.Text = utf_8ToHex(textBox2.Text, true);
+                }
+                else
+                {
+                    textBox1.Text = utf_8ToHex(textBox2.Text);
+                }
+            }
+            
+            
+        }
+    }
+}
